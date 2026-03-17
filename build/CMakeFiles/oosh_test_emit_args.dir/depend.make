71b07cbe1bd9d4b042d4f4bf3f057c15d2f665d0
Nicolò Risitano <cto@genomeup.com>
2026-03-17 23:02:15 +0100

    modifiche

---
+++ build/CMakeFiles/oosh_test_emit_args.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for oosh_test_emit_args.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/oosh_test_pause.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oosh_test_pause.dir/tests/pause_seconds.c.o"
+  "CMakeFiles/oosh_test_pause.dir/tests/pause_seconds.c.o.d"
+  "oosh_test_pause"
+  "oosh_test_pause.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/oosh_test_pause.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
